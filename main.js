1e1a414185b8d1d6084dc67ea659b11f7a91c1ff
marynnara <143749397+marynnara@users.noreply.github.com>
2024-04-26 19:45:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+//Percorrer (iterar) uma lista de elementos
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
+// document.querySelector("#minhaDiv p").textContent = "Chega de calor";
+const contadores = document.querySelectorAll(".contador");
+let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2025-01-10T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-17T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-18T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+
+for (let i = 0; i < contadores.length; i++) {
+  contadores[i].textContent = calculaTempo(tempos[i]);
+}
